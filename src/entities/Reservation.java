90b93f09f5c513049e32bd211e830ca73b0d08a6
Wellinton Sousa Pires <weltin.max@gmail.com>
2026-02-24 18:53:50 -0300

    Very bad example

---
+++ src/entities/Reservation.java
@@ -0,0 +1,58 @@
+package entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    private Integer roomNumber;
+    private Date checkIn;
+    private Date checkOut;
+
+    public Reservation(){
+    }
+
+    public Reservation(Integer roomNumber, Date checkin, Date checkout) {
+        this.roomNumber = roomNumber;
+        this.checkIn = checkin;
+        this.checkOut = checkout;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public Date getCheckin() {
+        return checkIn;
+    }
+
+    public Date getCheckout() {
+        return checkOut;
+    }
+
+    public long duration(){
+        long diff = checkOut.getTime() - checkIn.getTime();
+        return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+    }
+
+    public void updatesDates (Date checkIn, Date checkOut) {
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    @Override
+    public String toString(){
+        return "Reservation: Room " + roomNumber
+                + ", check-in " + sdf.format(getCheckin())
+                + ", check-out " + sdf.format(getCheckout())
+                + ", " + duration() + " nights";
+    }
+
+}
